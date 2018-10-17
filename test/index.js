e280fb276ff6dbb84e543015f038e27b25d71de2
m31271n <m31271n@users.noreply.github.com>
2018-10-17 17:46:05 +0800

    test: remove all tests

--- test/index.js
+++ test/index.js
@@ -1,9 +1,3 @@
 import test from 'ava'
-import fn from '../src'
 
-test('title', t => {
-  const err = t.throws(() => fn(123), TypeError)
-  t.is(err.message, 'Expected a string, got number')
-
-  t.is(fn('unicorns'), 'unicorns & rainbows')
-})
+test('no test', t => t.true(true))

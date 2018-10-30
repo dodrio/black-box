59ef0a2ecaa67ad55b2e567fc81773353a3b6a9f
m31271n <m31271n@users.noreply.github.com>
2018-10-31 00:44:36 +0800

    feat: add Input/SoundButton

---
+++ src/Input/SoundButton.js
@@ -0,0 +1,37 @@
+import { AssetManager, Component } from 'black-engine'
+
+const { default: defaultAssetManager } = AssetManager
+
+class SoundButton extends Component {
+  constructor(
+    downSoundName,
+    upSoundName,
+    { assetManager } = { assetManager: defaultAssetManager }
+  ) {
+    super()
+
+    this.mDownSoundClip =
+      (downSoundName && assetManager.getSound(downSoundName)) || null
+    this.mUpSoundClip =
+      (upSoundName && assetManager.getSound(upSoundName)) || null
+    this.mOnPointerUp = null
+    this.mOnPointerDown = null
+  }
+
+  onAdded(gameObject) {
+    this.mOnPointerDown = gameObject.on('pointerDown', () => {
+      this.mDownSoundClip.play()
+    })
+
+    this.mOnPointerUp = gameObject.on('pointerUp', () => {
+      this.mUpSoundClip.play()
+    })
+  }
+
+  onRemoved() {
+    this.mOnPointerDown.off()
+    this.mOnPointerUp.off()
+  }
+}
+
+export default SoundButton

7a5286fc6c0f0c0a8032400073b07d2c2e02ce2f
m31271n <m31271n@users.noreply.github.com>
2018-10-31 00:26:37 +0800

    feat: add three ease funcitons

---
+++ src/Animation/Ease.js
@@ -0,0 +1,18 @@
+class Ease {
+  // swipe
+  static outQuint(t) {
+    return 1 + --t * t * t * t * t
+  }
+
+  // swipe bounce
+  static outQuard(t) {
+    return t * (2 - t)
+  }
+
+  // bounce
+  static outQuart(t) {
+    return 1 - --t * t * t * t
+  }
+}
+
+export default Ease
